7bf436a350b7c32e2f923d8ce557f5d1e3e140e1
CodeMonster <goforlee@gmail.com>
2019-07-23 20:51:43 +0900

    7.23

--- nnb/src/main/java/ezen/nnb/main/MainController.java
+++ nnb/src/main/java/ezen/nnb/main/MainController.java
@@ -1,5 +1,29 @@
 package ezen.nnb.main;
 
-public class MainController {
+import java.util.List;
+import java.util.Map;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ezen.nnb.common.CommandMap;
+
+
+@Controller
+public class MainController {
+	
+		Logger log = Logger.getLogger(this.getClass());
+
+		@RequestMapping(value="/main")
+	    public ModelAndView main(CommandMap commandMap) throws Exception{
+	    	ModelAndView mv = new ModelAndView("/member/main");
+	    	return mv;
+	    }
 }
+		
+		
--- nnb/src/main/java/ezen/nnb/member/controller/HelpController.java
+++
@@ -1,5 +0,0 @@
-package ezen.nnb.member.controller;
-
-public class HelpController {
-
-}
