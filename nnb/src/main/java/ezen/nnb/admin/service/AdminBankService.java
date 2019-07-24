8cdc6948b1e8d1a2357df4407c200bb413622453
Administrator <Administrator@EZEN-1402-05>
2019-07-24 15:41:54 +0900

    admin bank

--- nnb/src/main/java/ezen/nnb/admin/service/AdminBankService.java
+++ nnb/src/main/java/ezen/nnb/admin/service/AdminBankService.java
@@ -1,5 +1,10 @@
 package ezen.nnb.admin.service;
 
+import java.util.List;
+import java.util.Map;
+
 public interface AdminBankService {
+
+	List<Map<String, Object>> selectBankList(Map<String, Object> commandMap);
 
 }
