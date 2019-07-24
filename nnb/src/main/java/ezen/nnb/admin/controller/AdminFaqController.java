16cee458ce8982c75cfb41845ec1f2e2a932f65d
yaongi <Ez-1401@Ez-1401-PC>
2019-07-24 17:58:57 +0900

    FAQ on progress

--- nnb/src/main/java/ezen/nnb/admin/controller/AdminFaqController.java
+++ nnb/src/main/java/ezen/nnb/admin/controller/AdminFaqController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import ezen.nnb.admin.service.AdminFaqService;
+import ezen.nnb.common.CommandMap;
 
 
 @Controller
@@ -19,16 +20,17 @@ public class AdminFaqController {
 	private AdminFaqService adminFaqService;
 
 	@RequestMapping(value = "/admin/faq/list")
-	public ModelAndView adminFaqList(Map<String, Object> commandMap) throws Exception {
+	public ModelAndView adminFaqList(CommandMap commandMap) throws Exception {
 		ModelAndView mv = new ModelAndView("/admin/faq/list");
-		List<Map<String, Object>> list = adminFaqService.selectFaqList(commandMap);
+		List<Map<String, Object>> list = adminFaqService.selectFaqList(commandMap.getMap());
 		mv.addObject("list", list);
 		return mv;
 	}
 	
 	@RequestMapping(value = "/admin/faq/detail")
-	public ModelAndView adminFaqDetail(Map<String, Object> commandMap) throws Exception {
+	public ModelAndView adminFaqDetail(CommandMap commandMap) throws Exception {
 		ModelAndView mv = new ModelAndView("/admin/faq/detail");
-		List<>
+		
+		return mv;
 	}
 }
