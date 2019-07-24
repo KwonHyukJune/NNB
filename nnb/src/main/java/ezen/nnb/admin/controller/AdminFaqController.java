ab6b8882dcc74478482156e093dcf1b359072a1c
yaongi <Ez-1401@Ez-1401-PC>
2019-07-24 12:43:40 +0900

    ADMIN FAQ LIST

--- nnb/src/main/java/ezen/nnb/admin/controller/AdminFaqController.java
+++ nnb/src/main/java/ezen/nnb/admin/controller/AdminFaqController.java
@@ -1,4 +1,28 @@
 package ezen.nnb.admin.controller;
 
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Resource;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ezen.nnb.admin.service.AdminFaqService;
+
+
+@Controller
 public class AdminFaqController {
+	Logger log = Logger.getLogger(this.getClass());
+	
+	@Resource(name = "adminFaqService")
+	private AdminFaqService adminFaqService;
+
+	@RequestMapping(value = "/admin/faq/list")
+	public ModelAndView openSampleBoardList(Map<String, Object> commandMap) throws Exception {
+		ModelAndView mv = new ModelAndView("/admin/faq/list");
+		List<Map<String, Object>> list = adminFaqService.selectFaqList(commandMap);
+		mv.addObject("list", list);
+		return mv;
+	}
 }
---
+++ nnb/src/main/java/ezen/nnb/admin/service/AdminFaqServiceImpl.java
@@ -0,0 +1,17 @@
+package ezen.nnb.admin.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service("adminFaqService")
+public class AdminFaqServiceImpl implements AdminFaqService{
+
+	@Override
+	public List<Map<String, Object>> selectFaqList(Map<String, Object> commandMap) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
