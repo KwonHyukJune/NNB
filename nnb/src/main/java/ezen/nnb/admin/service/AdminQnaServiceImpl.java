f6630958ad358b16b76702bb355ba0b86a049c46
yaongi <Ez-1401@Ez-1401-PC>
2019-07-26 11:14:02 +0900

    이웅희 7-26-11:13

--- nnb/src/main/java/ezen/nnb/admin/service/AdminQnaServiceImpl.java
+++ nnb/src/main/java/ezen/nnb/admin/service/AdminQnaServiceImpl.java
@@ -20,13 +20,13 @@ public class AdminQnaServiceImpl implements AdminQnaService{
 	@Override
 	public List<Map<String, Object>> selectQnaList(Map<String, Object> map) throws Exception {
 		// TODO Auto-generated method stub
-		return AdminQnaDAO.selectQnaList(map);
+		return adminQnaDAO.selectQnaList(map);
 	}
 
 	@Override
 	public Map<String, Object> selectQnaDetail(Map<String, Object> map) throws Exception {
 		// TODO Auto-generated method stub
-		return AdminQnaDAO.selectQnaDetail(map);
+		return adminQnaDAO.selectQnaDetail(map);
 	}
 
 }
