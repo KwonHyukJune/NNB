cbce55791be6c10507f493118469607df3f1f5fe
Administrator <Administrator@WIN-GA42JII2DKR>
2019-07-26 11:42:18 +0900

    injeong 2019 07 26 11:41

--- nnb/src/main/java/ezen/nnb/admin/service/AdminMemberService.java
+++ nnb/src/main/java/ezen/nnb/admin/service/AdminMemberService.java
@@ -1,5 +1,16 @@
 package ezen.nnb.admin.service;
 
-public class AdminMemberService {
+import java.util.List;
+import java.util.Map;
+
+public interface AdminMemberService {
+	public List<Map<String, Object>>adminMemberList(Map<String,Object>map)throws Exception;
+	
+	public Map<String,Object>adminMemberDetail(Map<String,Object>map)throws Exception;
+	
+	public void adminMemberBan(Map<String,Object>map)throws Exception;
+	
+	public List<Map<String,Object>>adminMemberSearch(Map<String, Object>map)throws Exception;
+	
 
 }
