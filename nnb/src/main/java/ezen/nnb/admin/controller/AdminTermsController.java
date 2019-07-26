19915b92af1976ce9123e5de9fc42891b6067ea4
Administrator <Administrator@EZEN-1402-05>
2019-07-26 15:40:37 +0900

    권혁준 7/26 3:40

--- nnb/src/main/java/ezen/nnb/admin/controller/AdminTermsController.java
+++ nnb/src/main/java/ezen/nnb/admin/controller/AdminTermsController.java
@@ -1,5 +1,82 @@
 package ezen.nnb.admin.controller;
 
-public class AdminTermsController {
+import java.util.List;
+import java.util.Map;
 
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ezen.nnb.admin.service.AdminTermsService;
+import ezen.nnb.common.CommandMap;
+
+public class AdminTermsController {
+Logger log = Logger.getLogger(this.getClass());
+	
+	@Resource(name = "adminTermsService")
+	private AdminTermsService adminTermsService;
+
+	@RequestMapping(value = "/admin/Terms/list")
+	public ModelAndView adminTermsList(CommandMap commandMap) throws Exception {
+		ModelAndView mv = new ModelAndView("/admin/Terms/list");
+		List<Map<String, Object>> list = adminTermsService.selectTermsList(commandMap.getMap());
+		mv.addObject("list", list);
+		return mv;
+	}
+
+	@RequestMapping(value="/admin/Terms/writeForm")
+	public ModelAndView adminTermsWriteForm(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("/admin/Terms/writeForm");
+		return mv;
+	}
+	
+	@RequestMapping(value="/admin/Terms/write")
+	public ModelAndView adminTermsWrite(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("redirect:/admin/Terms/writeForm");
+		
+		adminTermsService.writeTerms(commandMap.getMap());
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/admin/Terms/detail")
+	public ModelAndView adminTermsDetail(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("/admin/Terms/detail");
+		
+		Map<String,Object> map = adminTermsService.selectTermsDetail(commandMap.getMap());
+		mv.addObject("map", map);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/admin/Terms/modifyForm")
+	public ModelAndView adminTermsModifyForm(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("/admin/Terms/modifyForm");
+		
+		Map<String,Object> map = adminTermsService.selectTermsDetail(commandMap.getMap());
+		mv.addObject("map", map);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/admin/Terms/modify")
+	public ModelAndView adminTermsModify(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("redirect:/admin/Terms/Detail");
+		
+		adminTermsService.updateTermsModify(commandMap.getMap());
+		
+		mv.addObject("Terms_NUM", commandMap.get("Terms_NUM"));
+		return mv;
+	}
+	
+	@RequestMapping(value="/admin/Terms/delete")
+	public ModelAndView adminTermsDelete(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("redirect:/admin/Terms/list");
+		
+		adminTermsService.deleteTerms(commandMap.getMap());
+		
+		return mv;
+	}
 }
--- nnb/src/main/java/ezen/nnb/admin/dao/AdminTermsDAO.java
+++ nnb/src/main/java/ezen/nnb/admin/dao/AdminTermsDAO.java
@@ -1,5 +1,36 @@
 package ezen.nnb.admin.dao;
 
-public class AdminTermsDAO {
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.stereotype.Repository;
+
+import ezen.nnb.common.AbstractDAO;
+
+@Repository("adminTermsDAO")
+public class AdminTermsDAO extends AbstractDAO{
+
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectTermsList(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return (List<Map<String, Object>>) selectList("terms.selectTermsList", map);
+
+	}
+	public void writeTerms(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		insert("terms.insertTerms", map);
+	}
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> selectTermsDetail(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return (Map<String, Object>) selectOne("terms.selectTermsDetail", map);
+	}
+	public void updateTermsModify(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		update("terms.updateTermsModify", map);
+	}
+	public void deleteTerms(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		delete("terms.deleteTerms", map);
+	}
 }
