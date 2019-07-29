e3cf4882605f8778c936f0ba58293731930713d6
withfour <50561094+withfour@users.noreply.github.com>
2019-07-29 12:14:12 +0900

    Revert "Kimjiwoo"

--- nnb/src/main/java/ezen/nnb/admin/service/AdminBankServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package ezen.nnb.admin.service;
-
-import org.springframework.stereotype.Service;
-
-@Service("adminBankService")
-public class AdminBankServiceImpl implements AdminBankService{
-
-}
--- nnb/src/main/java/ezen/nnb/admin/service/AdminFaqService.java
+++ nnb/src/main/java/ezen/nnb/admin/service/AdminFaqService.java
@@ -1,10 +1,5 @@
 package ezen.nnb.admin.service;
 
-import java.util.List;
-import java.util.Map;
-
-public interface AdminFaqService {
-
-	List<Map<String, Object>> selectFaqList(Map<String, Object> map) throws Exception;
+public class AdminFaqService {
 
 }
