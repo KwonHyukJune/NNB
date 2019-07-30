b8353ae22d654ef26f223b78a7f0517c41f4d01b
EZEN-16 <EZEN-16@EZEN-16-PC>
2019-07-30 16:20:06 +0900

    Chan-xylo 7/30 16:00

--- nnb/src/main/java/ezen/nnb/admin/dao/AdminRoomDAO.java
+++ nnb/src/main/java/ezen/nnb/admin/dao/AdminRoomDAO.java
@@ -1,29 +1,41 @@
 package ezen.nnb.admin.dao;
+
 import java.util.List;
 import java.util.Map;
+
 import org.springframework.stereotype.Repository;
+
 import ezen.nnb.common.AbstractDAO;
 
-@Repository("adminRoomDAO")
+@Repository("AdminRoomDAO")
 public class AdminRoomDAO extends AbstractDAO{
-	
-	@SuppressWarnings("unchecked")
-	public List<Map<String, Object>> selectRoomList(Map<String, Object> map)throws Exception{
-	return (List<Map<String,Object>>)selectList("room.selectRoomList", map);
-	}
-	
-	@SuppressWarnings("unchecked")
-	public Map<String, Object> selectRoomDetail(Map<String, Object> map) throws Exception{
-	return (Map<String,Object>)selectOne("room.detailRoom",map);
-	}
-		
-	public void updateRoomModify(Map<String, Object> map) throws Exception{
-	update("room.updateRoom",map);
-	}
-	
-	public void deleteRoom(Map<String, Object> map) throws Exception {
-	update("room.deleteRoom",map);
-	}
-} 
 
-  
+	@SuppressWarnings("unchecked")
+	public List<Map<String,Object>>selectAdminRoomList(Map<String,Object>map) throws Exception{
+		return (List<Map<String,Object>>)selectList("adminRoom.selectAdminRoomList",map);
+	}
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> detailRoom(Map<String, Object> map) throws Exception{
+		return (Map<String,Object>) selectOne("room.detailRoom",map);
+	}
+
+	public void updateAdminRoomStatus(Map<String, Object> map) throws Exception {
+		update("adminRoom.updateAdminRoomStatus", map);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectAdminRoomTitle(Map<String, Object> map) throws Exception {
+		return (List<Map<String, Object>>) selectList("adminRoom.selectAdminRoomTitle", map);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectAdminRoomContent(Map<String, Object> map) throws Exception {
+		return (List<Map<String, Object>>) selectList("adminRoom.selectAdminRoomContent", map);
+	}
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectAdminRoomWriter(Map<String, Object> map) throws Exception {
+		return (List<Map<String, Object>>) selectList("adminRoom.selectAdminRoomWriter", map);
+	}
+	
+
+}
