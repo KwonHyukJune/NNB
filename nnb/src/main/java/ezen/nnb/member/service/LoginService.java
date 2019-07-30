d71ecf492bb9ce428c0b2ffbdf1a6b2d827b285e
yaongi <Ez-1401@Ez-1401-PC>
2019-07-30 12:59:54 +0900

    회원가입 이메일 인증 및 로그인 진행중: 이웅희 1:00pm

--- nnb/src/main/java/ezen/nnb/member/service/LoginService.java
+++ nnb/src/main/java/ezen/nnb/member/service/LoginService.java
@@ -6,4 +6,8 @@ public interface LoginService {
 
 	public Map<String, Object> loginCheck(Map<String, Object> map) throws Exception;
 
+	public Map<String, Object> findIdWithPhone(Map<String, Object> map) throws Exception;
+
+	public Map<String, Object> findIdWithEmail(Map<String, Object> map) throws Exception;
+
 }
