bd1888cebee565739c12aa17d896bb333e78d84d
Administrator <Administrator@WIN-GA42JII2DKR>
2019-08-05 16:51:13 +0900

    injeong 2019-08-05 4:51

--- nnb/src/main/java/ezen/nnb/member/controller/RoommateController.java
+++ nnb/src/main/java/ezen/nnb/member/controller/RoommateController.java
@@ -1,5 +1,223 @@
+
 package ezen.nnb.member.controller;
 
-public class RoommateController {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest; 
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import ezen.nnb.admin.paging.Paging;
+import ezen.nnb.common.CommandMap;
+import ezen.nnb.member.service.IgnoreService;
+import ezen.nnb.member.service.RoommateService;
+@Controller
+public class RoommateController {
+		
+@Resource(name="roommateService")
+private RoommateService roommateService;
+@Resource(name="ignoreService")
+private IgnoreService ignoreService;
+	int currentPage = 0;
+	int totalCount; 
+	int blockCount = 10;
+	int blockPage = 5;
+	private String pagingHtml;  
+	private Paging page;
+		
+
+	@RequestMapping(value="/roommate/search")
+	public ModelAndView searchRoommate(HttpServletResponse response,HttpServletRequest request,CommandMap commandMap)throws Exception{
+			ModelAndView mv=new ModelAndView("member/roommate/roommateList");
+			List<Map<String,Object>>searchRoommate=roommateService.searchRoommate(commandMap.getMap());	  
+			
+			String mateList=(String)commandMap.getMap().get("mateList");	
+			
+			//주소부분은 추가해야함(현재는 비워져 있음)
+			if((String)commandMap.getMap().get("ri_region")!=null) {
+				
+			}else {
+				
+			}
+			String MEM_NICK=request.getParameter("MEM_NICK");
+			if((String)commandMap.getMap().get("MEM_NICK")!=null) {
+				mv.addObject("MEM_NICK",MEM_NICK);
+			}else {
+				mv.addObject("searchRoommate",searchRoommate);			
+			}
+			String RI_AGE=request.getParameter("RI_AGE");
+			if((String)commandMap.getMap().get("RI_AGE")!=null) {
+				mv.addObject("RI_AGE",RI_AGE);
+				commandMap.put("minyear", "minyear");
+				commandMap.put("maxyear", "maxyear");
+			}else {
+				mv.addObject("searchRoommate",searchRoommate);
+				commandMap.put("minyear",1);//....??
+			}
+			String RI_GENDER=request.getParameter("RI_GENDER");
+			if((String)commandMap.getMap().get("RI_GENDER")!=null) {
+				mv.addObject("RI_GENDER",RI_GENDER);
+			}else {
+				mv.addObject("searchRoommate",searchRoommate);
+			}
+			String RI_LOAN_BIG=request.getParameter("RI_LOAN_BIG");
+			if((String)commandMap.getMap().get("RI_LOAN_BIG")!=null) {
+				mv.addObject("RI_LOAN_BIG",RI_LOAN_BIG);
+				commandMap.put("mindeposit", "mindeposit");
+				commandMap.put("maxdeposit", "maxdeposit");
+			}else {
+				mv.addObject("searchRoommate",searchRoommate);
+			}
+			
+		
+			//다중 및 개별 검색?
+			try {
+				if(mateList!=null) {
+					for(int i=0;i<mateList.length();i++) 
+						commandMap.getMap().put("mateList",mateList);
+				}
+			}catch(Exception e) {
+				if(mateList!=null) {
+					commandMap.getMap().put("mateList",mateList);
+				}
+			}
+			
+		
+	totalCount = searchRoommate.size();
+			
+			page = new Paging(currentPage, totalCount, blockCount, blockPage, "searchRoommate");
+			pagingHtml = page.getPagingHtml().toString();
+			
+			int lastCount = totalCount;
+			
+			if(page.getEndCount() < totalCount)
+				lastCount = page.getEndCount() + 1;
+			
+			searchRoommate = searchRoommate.subList(page.getStartCount(), lastCount);
+			
+			 mv.addObject("MEM_NICK",MEM_NICK);
+			 mv.addObject("RI_AGE",RI_AGE);
+			mv.addObject("RI_GENDER",RI_GENDER);
+			mv.addObject("RI_LOAN_BIG",RI_LOAN_BIG);
+			  mv.addObject("totalCount",totalCount);
+			  mv.addObject("pagingHtml",pagingHtml);
+			  mv.addObject("currentPage",currentPage);
+			  mv.addObject("searchRoommate",searchRoommate);
+			  mv.setViewName("member/roommate/roommateList");
+		
+			  return mv;
+	
+}
+		@RequestMapping(value="/roommate/list/addFavRoommate",method=RequestMethod.POST)
+		@ResponseBody
+		public ModelAndView listAddFavRoommate(HttpServletRequest request, HttpServletResponse response,CommandMap commandMap)throws Exception {
+			ModelAndView mv=new ModelAndView();
+			HttpSession session=request.getSession();
+			String check=request.getParameter("check");
+			commandMap.put("IGNORE_MEM",session.getAttribute("MEM_ID"));		
+			commandMap.put("IGNORE_NUM", commandMap.get("IGNORE_NUM"));
+			if(check=="1") {
+				roommateService.listAddFavRoommate(commandMap.getMap());
+				mv.setViewName("member/roommate/roommateList");
+			}
+			return mv;
+		}
+		
+	@RequestMapping(value="/roommate/list/deleteFavRoommate")
+	public ModelAndView listDeleteFavRoommate(CommandMap commandMap,HttpServletRequest request)throws Exception{
+		ModelAndView mv = new ModelAndView();
+		HttpSession session=request.getSession();
+		String check=request.getParameter("check");
+		commandMap.put("IGNORE_MEM",session.getAttribute("MEM_ID"));		
+		commandMap.put("IGNORE_NUM", commandMap.get("IGNORE_NUM"));
+		if(check=="0") {
+			roommateService.listDeleteFavRoommate(commandMap.getMap());
+			mv.setViewName("redirect:/roommate/search");
+		} 
+		return mv;
+		
+	}
+	@RequestMapping(value="/roommate/detail")
+	public ModelAndView openRoommateDetail(CommandMap commandMap)throws Exception{
+		ModelAndView mv=new ModelAndView("/member/roommate/roommateDetail");
+		Map<String,Object>map=roommateService.openRoommateDetail(commandMap.getMap());
+		mv.addObject("map",map.get("map"));
+		return mv;
+	}
+	
+	@RequestMapping(value="/roommate/detail/addFavRoommate",method=RequestMethod.POST)
+	@ResponseBody
+	public ModelAndView detailAddFavRoommate(HttpServletRequest request, HttpServletResponse response,CommandMap commandMap)throws Exception {
+		ModelAndView mv=new ModelAndView();
+		HttpSession session=request.getSession();
+		if(session.getAttribute("MEM_ID")!=null) {
+			commandMap.put("FAV_MATE_NUM",commandMap.get("FAV_MATE_NUM"));
+			commandMap.put("MEM_ID", session.getAttribute("MEM_ID"));
+			roommateService.listAddFavRoommate(commandMap.getMap());
+		}else {
+			mv.setViewName("redirect:/member/main/loginForm");
+		}
+		return mv;
+	}
+	
+@RequestMapping(value="/roommate/detail/deleteFavRoommate")
+public ModelAndView datailDeleteFavRoommate(CommandMap commandMap)throws Exception{
+	ModelAndView mv = new ModelAndView();
+	roommateService.detailDeleteFavRoommate(commandMap.getMap());
+	mv.setViewName("redirect:/roommate/detail");
+	return mv;
+		
+}
+@RequestMapping(value="/roommate/detail/messageWriteForm")
+public ModelAndView messageWriteForm()throws Exception{
+	ModelAndView mv=new ModelAndView();
+	mv.setViewName("/member/myPage/messageWriteForm");
+	return mv;
+}
+@RequestMapping(value="/roommate/detail/messageWrite",method=RequestMethod.POST)
+public ModelAndView messageWrite(CommandMap commandMap, HttpServletRequest request) throws Exception{
+	ModelAndView mv=new ModelAndView();
+	HttpSession session = request.getSession();
+	
+	if (session.getAttribute("RECEIVER")!=null){
+		commandMap.put("MESSAGE_NUM", commandMap.get("MESSAGE_NUM"));
+		commandMap.put("SENDER", session.getAttribute("MEM_ID"));		
+		mv.setViewName("member/myPage/messageWrite");
+	}else {
+		mv.setViewName("redirect:/member/roommate/roommateDetail");
+	}
+		 
+return mv;
+}
+@RequestMapping(value="/roommate/ignore")
+public ModelAndView ignoreUser(CommandMap commandMap, HttpServletRequest request) throws Exception{
+	ModelAndView mv=new ModelAndView("redirect:/member/roommate/roommateDetail");
+	HttpSession session = request.getSession();
+	int check = ignoreService.checkIgnore(commandMap.getMap());
+		commandMap.put("IGNORE_MEM",session.getAttribute("MEM_ID"));		
+		commandMap.put("IGNORE_NUM", commandMap.get("IGNORE_NUM"));
+	if(check==0) {
+		ignoreService.insertIgnore(commandMap.getMap());
+	}
+return mv;
+}
+
+@RequestMapping(value="/roommate/ignoreUser")
+public ModelAndView unIgnoreUser(CommandMap commandMap) throws Exception{
+	ModelAndView mv=new ModelAndView("redirect:/member/roommate/roommateDetail");
+	int check= ignoreService.checkIgnore(commandMap.getMap());
+	if(check==1) {
+		ignoreService.insertIgnore(commandMap.getMap());
+		}
+	return mv;
+	}
 }
