0b1cd0791f6605861632445f944503ba7d4889ca
woonghee <woonghee@218.38.137.27>
2019-08-09 10:39:05 +0900

    웅

--- nnb/src/main/java/ezen/nnb/member/controller/RoommateInfoController.java
+++ nnb/src/main/java/ezen/nnb/member/controller/RoommateInfoController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,12 @@ public class RoommateInfoController {
 	private RoommateService roommateService;
 	
 	@RequestMapping(value="/myPage/myProfile")
-	public ModelAndView openRoommateDetail(CommandMap commandMap)throws Exception{
+	public ModelAndView openRoommateDetail(CommandMap commandMap,HttpServletRequest request)throws Exception{
 		ModelAndView mv=new ModelAndView("/member/myPage/myProfile");
+		HttpSession session= request.getSession();
+		String MEM_ID = (String)session.getAttribute("MEM_ID");
+		commandMap.put("MEM_ID",MEM_ID);
+		
 		Map<String,Object>map=roommateService.openRoommateDetail(commandMap.getMap());
 		mv.addObject("map",map);
 		return mv;
